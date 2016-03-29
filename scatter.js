4f70a22ff8007abdbb42979eedf279805bd32ae4
Omar Mujahid <omar_mjhd@yahoo.com>
2016-03-29 15:42:47 -0400

    First commit with functional code

---
+++ scatter.js
@@ -0,0 +1,189 @@
+var margin = {top: 50, right: 0, bottom: 30, left: 0};
+var width = 400 - margin.left - margin.right;
+var height = 300 - margin.top - margin.bottom;
+
+// pre-cursors
+var sizeForCircle = function(d) {
+    // TODO: modify the size
+    return 5;
+};
+
+// setup x
+var xValue = function(d) { return d.Assists;}, // data -> value
+    xScale = d3.scale.linear().range([0, width]), // value -> display
+    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+// setup y
+var yValue = function(d) { return d.EFG;}, // data -> value
+    yScale = d3.scale.linear().range([height, 0]), // value -> display
+    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+// setup fill color
+var cValue = function(d) { return d.Manufacturer;},
+    color = d3.scale.category10();
+
+// add the graph canvas to the body of the webpage
+var scatter = d3.select(".scatter-plot").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var bar = d3.select(".barchart").append("svg")
+    .attr("width", width + 250 + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+var nutrition = d3.select(".nutrition-label").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+
+// add the tooltip area to the webpage
+var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+// load data
+d3.csv("Results.csv", function(error, data) {
+
+    // change string (from CSV) into number format
+    data.forEach(function(d) {
+        d.Assists = +d.Assists;
+        d.EFG = +d.EFG;
+    });
+
+    //console.log(data);
+    //manufacturer, EFG, number of cereals
+
+    //SCATTER PLOT START
+    // don't want dots overlapping axis, so add in buffer to data domain
+    xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
+    yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+    // x-axis
+    scatter.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .attr("fill", "white")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .attr("fill", "white")
+        .style("text-anchor", "end")
+        .text("Assists");
+
+    // y-axis
+    scatter.append("g")
+        .attr("class", "y axis")
+        .attr("fill", "white")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .attr("fill", "white")
+        .style("text-anchor", "end")
+        .text("EFG");
+
+    // draw dots
+    scatter.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", sizeForCircle)
+        .attr("cx", xMap)
+        .attr("cy", yMap)
+        .style("fill", function(d) { return color(cValue(d));})
+        .on("mouseover", function(d) {
+
+            // TODO: show the tool tip
+            tooltip.style("opacity", 1);
+
+            // TODO: fill to the tool tip with the appropriate data
+            tooltip.html(d["Cereal Name"])
+                .style("left", d3.event.pageX + 5 + "px")
+                .style("top", d3.event.pageY + 5 + "px");
+
+            // TODO: ADD DELAY
+            d3.selectAll(".dot").transition()
+                .duration(300)
+                .attr("r", function(e) {
+
+                    if (d["Cereal Name"] == e["Cereal Name"]) {
+                        return 10 * e["Serving Size Weight"];
+                    } else {
+                        return 5 * e["Serving Size Weight"];
+                    }
+
+                });
+
+        })
+        .on("mouseout", function(d) {
+            // TODO: hide the tooltip
+            tooltip.style("opacity", 0);
+
+            // TODO: resize the nodes
+            d3.selectAll(".dot").transition()
+                .duration(500)
+                .attr("r", sizeForCircle);
+
+        })
+        .on("click", function(d) {
+
+            d3.selectAll(".bar").transition()
+                .duration(500)
+                .attr("fill", function(e) {
+
+                    if (d["EFG"] < e["Average"]) {
+                        return "white";
+                    } else {
+                        return color(cValue(e));;
+                    }
+
+                });
+
+        });
+
+    // draw legend
+    var legend = nutrition.selectAll(".legend")
+        .data(color.domain())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    // draw legend colored rectangles
+    legend.append("rect")
+        .attr("x", 20)
+        .attr("y", 0)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    // draw legend text
+    legend.append("text")
+        .attr("x", 45)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .attr("fill", "white")
+        .style("text-anchor", "begin")
+        .text(function(d) { return d;});
+
+    var xScaleBar = d3.scale.linear().range([0, width + 250]);
+    var yScaleBar = d3.scale.ordinal().rangeRoundBands([0, 300 - 30], 0.3);
+
+    var yAxisBar = d3.svg.axis().scale(yScaleBar).orient('left');
+    var xAxisBar = d3.svg.axis().scale(xScaleBar).orient('bottom');
+
+    xScaleBar.domain([0, d3.max(data, function(d){
+        return d["EFG"]
+    })]);
+
+    yScaleBar.domain(data.map(function(d) {
+        return d["Manufacturer"];
+    }));
+});
