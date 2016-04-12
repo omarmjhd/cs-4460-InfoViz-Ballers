c9f22dccc170b8886de97f0992bcea2a2f345247
Omar Mujahid <omar_mjhd@yahoo.com>
2016-04-12 16:14:25 -0400

    Filter function created for parallel coordinates

--- parallel.js
+++ parallel.js
@@ -1,141 +1,167 @@
-var margin = {top: 30, right: 10, bottom: 10, left: 10},
-    width = 960 - margin.left - margin.right,
-    height = 350 - margin.top - margin.bottom;
+function parallel(filterArray) {
+    var margin = {top: 30, right: 10, bottom: 10, left: 10},
+        width = 960 - margin.left - margin.right,
+        height = 350 - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal().rangePoints([0, width], 1),
-    y = {},
-    dragging = {};
+    var x = d3.scale.ordinal().rangePoints([0, width], 1),
+        y = {},
+        dragging = {};
 
-var color = d3.scale.ordinal()
-    .domain(['ATL', 'BOS', 'BRK', 'CHI', 'CLE', 'DAL', 'DEN', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA', 'OKC',
-        'ORL', 'PHO', 'POR', 'SAS', 'TOR'])
-    .range(['#E03A3E', '#008348', '#000000', '#CE1141', '#860038', '#007DC5', '#4FA8FF', '#006BB6', '#CE1141',
-        '#FFC633', '#ED174C', '#552582', '#BBD1E4', '#98002E', '#F05133', '#007DC5', '#E56020', '#F0163A',
-        '#B6BFBF', '#CE1141']);
+    var color = d3.scale.ordinal()
+        .domain(['ATL', 'BOS', 'BRK', 'CHI', 'CLE', 'DAL', 'DEN', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA', 'OKC',
+            'ORL', 'PHO', 'POR', 'SAS', 'TOR'])
+        .range(['#E03A3E', '#008348', '#000000', '#CE1141', '#860038', '#007DC5', '#4FA8FF', '#006BB6', '#CE1141',
+            '#FFC633', '#ED174C', '#552582', '#BBD1E4', '#98002E', '#F05133', '#007DC5', '#E56020', '#F0163A',
+            '#B6BFBF', '#CE1141']);
 
-var line = d3.svg.line(),
-    axis = d3.svg.axis().orient("left"),
-    background,
-    foreground;
+    var line = d3.svg.line(),
+        axis = d3.svg.axis().orient("left"),
+        background,
+        foreground;
 
-console.log(d3.select(".parallel-coordinate"));
+    console.log(d3.select(".parallel-coordinate"));
 
-var svg = d3.select(".parallel-coordinate").append("svg")
-    .attr("width", width     + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select(".parallel-coordinate").append("svg")
+        .attr("width", width     + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-console.log("SVG Parallel" + svg);
+    console.log("SVG Parallel" + svg);
 
-d3.csv("Results.csv", function(error, results) {
+    d3.csv("Results.csv", function(error, results) {
 
-    console.log("Beginning chart for parallel");
-    // Extract the list of dimensions and create a scale for each.
-    x.domain(dimensions = d3.keys(results[0]).filter(function(d) {
-        
-        return d != "Year" && d != "Team" && (y[d] = d3.scale.linear()
-                .domain(d3.extent(results, function(p) { return +p[d]; }))
-                .range([height, 0]));
-    }));
+        console.log("Beginning chart for parallel");
+        // Extract the list of dimensions and create a scale for each.
+        x.domain(dimensions = d3.keys(results[0]).filter(function(d) {
 
-    // Add grey background lines for context.
-    background = svg.append("g")
-        .attr("class", "background")
-        .selectAll("path")
-        .data(results)
-        .enter().append("path")
-        .attr("d", path);
+            return d != "Year" && d != "Team" && (y[d] = d3.scale.linear()
+                    .domain(d3.extent(results, function(p) { return +p[d]; }))
+                    .range([height, 0]));
+        }));
 
-    // Add blue foreground lines for focus.
-    foreground = svg.append("g")
-        .attr("class", "foreground")
-        .selectAll("path")
-        .data(results)
-        .enter().append("path")
-        .attr("d", path)
-        .attr('stroke', function(d) { return color(d["Team"]); });
+        // Add grey background lines for context.
+        background = svg.append("g")
+            .attr("class", "background")
+            .selectAll("path")
+            .data(results)
+            .enter().append("path")
+            .attr("d", path);
 
-    // Add a group element for each dimension.
-    var g = svg.selectAll(".dimension")
-        .data(dimensions)
-        .enter().append("g")
-        .attr("class", "dimension")
-        .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-        .call(d3.behavior.drag()
-            .origin(function(d) { return {x: x(d)}; })
-            .on("dragstart", function(d) {
-                dragging[d] = x(d);
-                background.attr("visibility", "hidden");
+        // Add colored foreground lines for focus.
+        foreground = svg.append("g")
+            .attr("class", "foreground")
+            .selectAll("path")
+            .data(results)
+            .enter().append("path")
+            .filter(function(d) { return contains(allTeams, d["Team"]); }) //need to create an array that has strings of teams based on user dropdown selection
+            .attr("d", path)
+            .attr('stroke', function(d) { return color(d["Team"]); });
+
+        // Add a group element for each dimension.
+        var g = svg.selectAll(".dimension")
+            .data(dimensions)
+            .enter().append("g")
+            .attr("class", "dimension")
+            .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+            .call(d3.behavior.drag()
+                .origin(function(d) { return {x: x(d)}; })
+                .on("dragstart", function(d) {
+                    dragging[d] = x(d);
+                    background.attr("visibility", "hidden");
+                })
+                .on("drag", function(d) {
+                    dragging[d] = Math.min(width, Math.max(0, d3.event.x));
+                    foreground.attr("d", path);
+                    dimensions.sort(function(a, b) { return position(a) - position(b); });
+                    x.domain(dimensions);
+                    g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
+                })
+                .on("dragend", function(d) {
+                    delete dragging[d];
+                    transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+                    transition(foreground).attr("d", path);
+                    background
+                        .attr("d", path)
+                        .transition()
+                        .delay(500)
+                        .duration(0)
+                        .attr("visibility", null);
+                }));
+
+        // Add an axis and title.
+        g.append("g")
+            .attr("class", "axis")
+            .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+            .append("text")
+            .style("text-anchor", "middle")
+            .attr("y", -9)
+            .text(function(d) { return d; });
+
+        // Add and store a brush for each axis.
+        g.append("g")
+            .attr("class", "brush")
+            .each(function(d) {
+                d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
             })
-            .on("drag", function(d) {
-                dragging[d] = Math.min(width, Math.max(0, d3.event.x));
-                foreground.attr("d", path);
-                dimensions.sort(function(a, b) { return position(a) - position(b); });
-                x.domain(dimensions);
-                g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
-            })
-            .on("dragend", function(d) {
-                delete dragging[d];
-                transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
-                transition(foreground).attr("d", path);
-                background
-                    .attr("d", path)
-                    .transition()
-                    .delay(500)
-                    .duration(0)
-                    .attr("visibility", null);
-            }));
-
-    // Add an axis and title.
-    g.append("g")
-        .attr("class", "axis")
-        .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-        .append("text")
-        .style("text-anchor", "middle")
-        .attr("y", -9)
-        .text(function(d) { return d; });
-
-    // Add and store a brush for each axis.
-    g.append("g")
-        .attr("class", "brush")
-        .each(function(d) {
-            d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
-        })
-        .selectAll("rect")
-        .attr("x", -8)
-        .attr("width", 16);
+            .selectAll("rect")
+            .attr("x", -8)
+            .attr("width", 16);
 
 
-    console.log("Done chart for parallel");
-});
+        console.log("Done chart for parallel");
+    });
 
-function position(d) {
-    var v = dragging[d];
-    return v == null ? x(d) : v;
-}
+    function position(d) {
+        var v = dragging[d];
+        return v == null ? x(d) : v;
+    }
 
-function transition(g) {
-    return g.transition().duration(500);
-}
+    function transition(g) {
+        return g.transition().duration(500);
+    }
 
 // Returns the path for a given data point.
-function path(d) {
-    return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
-}
+    function path(d) {
+        return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+    }
 
-function brushstart() {
-    d3.event.sourceEvent.stopPropagation();
-}
+    function brushstart() {
+        d3.event.sourceEvent.stopPropagation();
+    }
 
 // Handles a brush event, toggling the display of foreground lines.
-function brush() {
-    var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
-        extents = actives.map(function(p) { return y[p].brush.extent(); });
-    foreground.style("display", function(d) {
-        return actives.every(function(p, i) {
-            return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-        }) ? null : "none";
-    });
+    function brush() {
+        var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
+            extents = actives.map(function(p) { return y[p].brush.extent(); });
+
+        foreground.style("display", function(d) {
+            return actives.every(function(p, i) {
+                return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+            }) ? null : "none";
+        });
+    }
+
+    function contains(array, string) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] === string) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
+
+
+var allTeams = ['ATL', 'BOS', 'BRK', 'CHI', 'CLE', 'DAL', 'DEN', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA',
+    'OKC', 'ORL', 'PHO', 'POR', 'SAS', 'TOR'];
+
+var missingSA = ['ATL', 'BOS', 'BRK', 'CHI', 'CLE', 'DAL', 'DEN', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA',
+    'OKC', 'ORL', 'POR', 'SAS', 'TOR'];
+
+var redTeams = ['ATL', 'CHI', 'CLE', 'HOU', 'MIA', 'POR'];
+
+parallel(allTeams);
